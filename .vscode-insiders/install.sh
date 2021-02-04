5e910ea757b68a41a5df9e31a5346fe9288babca
okapy <36099852+okadrive@users.noreply.github.com>
2021-02-05 01:27:56 +0900

    update: .vscode->.vscode-insiders

--- .vscode-insiders/install.sh
+++ .vscode-insiders/install.sh
@@ -1,11 +1,11 @@
 # 設定を共有したいマシンで予めインストール済みのエクステンションを書き出しておく
 # $code --list-extensions > extensions.txt
 
-# vscodeがインストールされている場合のみ
+#.vscodeがインストールされている場合のみ
 #if [ -e ~/Library/Application\ Support/Code/User ]; then
 #    cd ~/Library/Application\ Support/Code/User
 
-# for M1 Mac: vscode insiders
+# for M1 Mac: .vscode-insiders
 if [ -e ~/Library/Application\ Support/Code\ -\ Insiders/User ]; then
     cd ~/Library/Application\ Support/Code\ -\ Insiders/User
     # 前の設定をバックアップ
@@ -13,15 +13,14 @@ if [ -e ~/Library/Application\ Support/Code\ -\ Insiders/User ]; then
     mv keybindings.json keybindings.json.bak
     mv snippets snippets.bak
 
-    # ~/dotfiles/.vscode以下に他マシンと共有したい設定ファイルが置かれている想定
-    #
+    # ~/dotfiles/.vscode-insiders以下に他マシンと共有したい設定ファイルが置かれている想定
     # 元の設定ファイルの代わりにシンボリックリンクを置いておく
-    ln -is ~/dotfiles/.vscode/settings.json
-    ln -is ~/dotfiles/.vscode/keybindings.json
-    ln -is ~/dotfiles/.vscode/snippets
+    ln -is ~/dotfiles/.vscode-insiders/settings.json
+    ln -is ~/dotfiles/.vscode-insiders/keybindings.json
+    ln -is ~/dotfiles/.vscode-insiders/snippets
 
     # 他マシンで書き出されたエクステンション一覧を使ってインストール
-    for extension in `cat ~/dotfiles/.vscode/extensions.txt`; do
+    for extension in `cat ~/dotfiles/.vscode-insiders/extensions.txt`; do
         #code --install-extension $extension
         code-insiders --install-extension $extension
     done
